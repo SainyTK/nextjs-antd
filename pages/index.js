0c2fa649c7232eb2269da453c692894a1338d983
tanakorn0314 <tanakorn0412@gmail.com>
2019-03-20 17:04:13 +0700

    withTheme

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Button } from 'antd';
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
+import withTheme from '../hocs/withTheme';
 
 const Box = styled.div`
     width: 100px;
     height: 100px;
-    background-color: ${palette('primary',1)};
+    background-color: ${palette('primary',0)};
     text-align: center;
 `;
 
-export default () => (
+export default withTheme(() => (
     <Box>
         Hello World
         <Button>Test Test</Button>
     </Box>
-);
+));
